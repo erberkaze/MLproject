75d0673752ceda4a43f0f2fc82b81dc80e9c67e5
erberkaze <46199884+erberkaze@users.noreply.github.com>
2021-11-25 08:48:50 +0300

    Directory and file update

--- tobool.py
+++ tobool.py
@@ -2,9 +2,11 @@ import pandas as pd
 import xlrd
 import os
 
+os.chdir("C:/Users/erenb/Desktop/Pro/NewDatas/wBool")
 for root, directories, files in os.walk('C:/Users/erenb/Desktop/Pro/NewDatas/'):
     for filename in files:
         df = pd.read_excel('C:/Users/erenb/Desktop/Pro/NewDatas/' + filename, engine='openpyxl')
         df['bug'] = df['bug'].astype('bool')
-        print(df)
-    
+        df.to_excel("New_" + filename)
+
+
